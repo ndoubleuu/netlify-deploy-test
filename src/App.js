803b731b1ccd4c5a4dfac910e1d1ca758277da08
Nicole <nicolewu.ncw@gmail.com>
2021-05-17 14:36:43 -0400

    nbd, just added some of my favourite emojis and complained about how react is hard to learn

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Text from './Some-Component.js'
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia est placeat numquam possimus vitae exercitationem a, et quisquam sapiente voluptatem delectus, adipisci reprehenderit debitis illo repudiandae consequatur dolorum sunt impedit? Vero soluta nam itaque tenetur porro sit iure nisi, sunt officia harum eos repudiandae maxime ratione a beatae autem voluptatibus id nemo qui? Excepturi sint, voluptas voluptatibus eveniet expedita maiores doloremque aperiam! Quas dolorem pariatur itaque in corrupti? Doloribus unde voluptas eum tenetur maxime adipisci hic ad laborum, suscipit atque doloremque similique itaque vitae, sunt placeat quam obcaecati temporibus ut. In deserunt voluptas et, eligendi reiciendis laborum laudantium iste iure doloremque inventore assumenda omnis laboriosam quam sit ab placeat nihil velit vitae dolor praesentium molestiae! Placeat quis suscipit laborum harum labore amet, voluptatem maiores adipisci quasi facere doloremque, id laudantium delectus aliquid ipsum sed quia. Laborum consequatur ducimus at harum reiciendis iusto enim recusandae earum expedita eius doloribus est, eos voluptas nobis tempora dicta blanditiis iure, ullam ut? Modi, ut. Libero animi vero assumenda nisi vel? Suscipit id, blanditiis consequuntur praesentium excepturi quam aliquid maxime dolorum unde possimus officia temporibus, nobis ullam at ipsam quidem, quibusdam totam! Doloremque nisi assumenda, eius reprehenderit laudantium expedita modi explicabo eaque, nostrum vel accusantium.
       </p>
+      <Text />
     </div>
   );
 }
---
+++ src/Some-Component.js
@@ -0,0 +1,22 @@
+import { Fragment } from 'react';
+
+const Text = () => {
+    return (
+        <Fragment>
+            <p>This is more text, but coming from a Component, because React is cool af, but also hard af to get the hang of. 
+            </p>
+            <ul>
+                <p>Here is a list of my favourite emojis:</p>
+                <li>🦦</li>
+                <li>🐙</li>
+                <li>🦑</li>
+                <li>🦐</li>
+                <li>🦫</li>
+                <li>🌶</li>
+            </ul>
+        </Fragment>
+    )
+}
+
+export default Text;
+
